4ec997bafccfc0edcbeaf21467d09c52fcad48f3
Briguy52 <Briguy52@gmail.com>
2016-02-02 12:44:25 -0500

    Init commit

---
+++ search.py
@@ -0,0 +1,151 @@
+# search.py
+# ---------
+# Licensing Information: Please do not distribute or publish solutions to this
+# project. You are free to use and extend these projects for educational
+# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
+# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
+
+"""
+In search.py, you will implement generic search algorithms which are called 
+by Pacman agents (in searchAgents.py).
+"""
+
+import util
+
+class SearchProblem:
+  """
+  This class outlines the structure of a search problem, but doesn't implement
+  any of the methods (in object-oriented terminology: an abstract class).
+  
+  You do not need to change anything in this class, ever.
+  """
+  
+  def getStartState(self):
+     """
+     Returns the start state for the search problem 
+     """
+     util.raiseNotDefined()
+    
+  def isGoalState(self, state):
+     """
+       state: Search state
+    
+     Returns True if and only if the state is a valid goal state
+     """
+     util.raiseNotDefined()
+
+  def getSuccessors(self, state):
+     """
+       state: Search state
+     
+     For a given state, this should return a list of triples, 
+     (successor, action, stepCost), where 'successor' is a 
+     successor to the current state, 'action' is the action
+     required to get there, and 'stepCost' is the incremental 
+     cost of expanding to that successor
+     """
+     util.raiseNotDefined()
+
+  def getCostOfActions(self, actions):
+     """
+      actions: A list of actions to take
+ 
+     This method returns the total cost of a particular sequence of actions.  The sequence must
+     be composed of legal moves
+     """
+     util.raiseNotDefined()
+           
+
+def tinyMazeSearch(problem):
+  """
+  Returns a sequence of moves that solves tinyMaze.  For any other
+  maze, the sequence of moves will be incorrect, so only use this for tinyMaze
+  """
+  from game import Directions
+  s = Directions.SOUTH
+  w = Directions.WEST
+  return  [s,s,w,s,w,w,s,w]
+
+def depthFirstSearch(problem):
+  """
+  Search the deepest nodes in the search tree first
+  [2nd Edition: p 75, 3rd Edition: p 87]
+  
+  Your search algorithm needs to return a list of actions that reaches
+  the goal.  Make sure to implement a graph search algorithm 
+  [2nd Edition: Fig. 3.18, 3rd Edition: Fig 3.7].
+  
+  To get started, you might want to try some of these simple commands to
+  understand the search problem that is being passed in:
+  
+  print "Start:", problem.getStartState() -> Start: (5,5) // this is a tuple 
+  print "Is the start a goal?", problem.isGoalState(problem.getStartState()) -> Is the start a goal? False
+  print "Start's successors:", problem.getSuccessors(problem.getStartState()) -> Start's successors: [((5, 4), 'South', 1), ((4, 5), 'West', 1)]
+
+  """
+  "*** YOUR CODE HERE ***"
+  from game import Directions
+  S = Directions.SOUTH
+  W = Directions.WEST
+  N = Directions.NORTH
+  E = Directions.EAST
+
+  visited = [] # a list of visited states (tuples) with no duplicates!
+  stack = util.Stack() # a stack of successors
+  moves = [] # a list of directions to get from start to goal (what this method will return)
+  
+  for i in problem.getSuccessors(problem.getStartState()):
+      stack.push(i)
+  
+  while(not stack.isEmpty()):
+       current = stack.pop() # current is a tuple of things
+       state = current[0]
+       if not problem.getSuccessors(current[0]) and not problem.isGoalState(current[0]):
+           diff = stack.pop()
+           stepsBack = abs(diff[0][0]-current[0][0]) + abs(diff[0][1]-current[0][1]) - 1
+           for i in range(stepsBack):
+               moves.pop()
+       
+       visited.append(current[0])
+       moves.append(current[1][0]) # append first char of direction name
+       options = problem.getSuccessors(current[0]) # options is a list of choices
+       for i in options:
+           stack.push(i)
+       if problem.isGoalState(current[0]):
+           print(moves)
+           
+  
+  util.raiseNotDefined()
+
+def breadthFirstSearch(problem):
+  """
+  Search the shallowest nodes in the search tree first.
+  [2nd Edition: p 73, 3rd Edition: p 82]
+  """
+  "*** YOUR CODE HERE ***"
+  util.raiseNotDefined()
+      
+def uniformCostSearch(problem):
+  "Search the node of least total cost first. "
+  "*** YOUR CODE HERE ***"
+  util.raiseNotDefined()
+
+def nullHeuristic(state, problem=None):
+  """
+  A heuristic function estimates the cost from the current state to the nearest
+  goal in the provided SearchProblem.  This heuristic is trivial.
+  """
+  return 0
+
+def aStarSearch(problem, heuristic=nullHeuristic):
+  "Search the node that has the lowest combined cost and heuristic first."
+  "*** YOUR CODE HERE ***"
+  util.raiseNotDefined()
+    
+  
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
