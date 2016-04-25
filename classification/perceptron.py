fcccfa61d25b66c3e62ab6b96de45d90012aa58b
Briguy52 <Briguy52@gmail.com>
2016-04-24 23:36:58 -0400

    woo it works

--- classification/perceptron.py
+++ classification/perceptron.py
@@ -13,7 +13,7 @@ PRINT = True
 class PerceptronClassifier:
   """
   Perceptron classifier.
-  
+
   Note that the variable 'datum' in this code refers to a counter of features
   (not to a raw samples.Datum).
   """
@@ -28,35 +28,54 @@ class PerceptronClassifier:
   def setWeights(self, weights):
     assert len(weights) == len(self.legalLabels);
     self.weights == weights;
-      
+
   def train( self, trainingData, trainingLabels, validationData, validationLabels ):
     """
     The training loop for the perceptron passes through the training data several
     times and updates the weight vector for each label based on classification errors.
-    See the project description for details. 
-    
-    Use the provided self.weights[label] data structure so that 
+    See the project description for details.
+
+    Use the provided self.weights[label] data structure so that
     the classify method works correctly. Also, recall that a
     datum is a counter from features to values for those features
     (and thus represents a vector a values).
     """
-    
+
     self.features = trainingData[0].keys() # could be useful later
     # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING, OR
     # THE AUTOGRADER WILL LIKELY DEDUCT POINTS.
-    
+
     for iteration in range(self.max_iterations):
       print "Starting iteration ", iteration, "..."
       for i in range(len(trainingData)):
-          "*** YOUR CODE HERE ***"
-          util.raiseNotDefined()
-    
+          # TODO: find and compare labels with 'true label'
+          # From question: y' = argMax(score(f,y'')) = myMax
+          # trainingData = practice problems
+          # trainingLabels = answer key for those problems
+          currMax, maxLabel = 0, 0
+          currInput = trainingData[i]
+          for label in self.legalLabels:
+              currWeight = self.weights[label]
+              score = currInput * currWeight
+              if score > currMax:
+                  currMax = max(score, currMax)
+                  maxLabel = label
+
+          # TODO: update prototypes
+          # w^(y) = w^(y) + f
+          # w^(y') = w^(y') - f
+
+          trueLabel = trainingLabels[i]
+          if trueLabel != maxLabel:
+            self.weights[trueLabel] = self.weights[trueLabel] + currInput
+            self.weights[maxLabel] = self.weights[maxLabel] - currInput
+
   def classify(self, data ):
     """
     Classifies each datum as the label that most closely matches the prototype vector
     for that label.  See the project description for details.
-    
-    Recall that a datum is a util.counter... 
+
+    Recall that a datum is a util.counter...
     """
     guesses = []
     for datum in data:
@@ -66,7 +85,7 @@ class PerceptronClassifier:
       guesses.append(vectors.argMax())
     return guesses
 
-  
+
   def findHighWeightFeatures(self, label):
     """
     Returns a list of the 100 features with the greatest weight for some label
@@ -77,4 +96,3 @@ class PerceptronClassifier:
     util.raiseNotDefined()
 
     return featuresWeights
-
--- classification/svmClassifier.py
+++ classification/svmClassifier.py
@@ -21,17 +21,17 @@ class SupportVectorMachineClassifier(classificationMethod.ClassificationMethod):
 
   def formattingData(self, data, features, mean=None, std=None):
     '''
-    Convert data in the form of a list of dict to a n x p list of list object, 
+    Convert data in the form of a list of dict to a n x p list of list object,
     which is used by scikit-learn. n is the number of training samples and p is the number of features.
     And normalize data with given mean and std so that each feature has mean 0 and std 1.
 
     features: the list of features
 
-    mean, std: both are dictionaries indexed by features. 
+    mean, std: both are dictionaries indexed by features.
     If mean==None, mean is set to all zero; if std==None, std is set to all one.
 
-    * Hint: 
-    Normalizing each feature to have mean 0 and sd 1 can enhance the performance of SVM. 
+    * Hint:
+    Normalizing each feature to have mean 0 and sd 1 can enhance the performance of SVM.
     '''
     if mean==None:
       mean = dict((u, 0.0) for u in features)
@@ -49,7 +49,7 @@ class SupportVectorMachineClassifier(classificationMethod.ClassificationMethod):
 
     You may need to call formattingData(...) to convert trainingData to an appropriate form for scikit-learn.
 
-    trainingData: A list [datum_1, datum_2, ...], with datum_i being i-th sample. 
+    trainingData: A list [datum_1, datum_2, ...], with datum_i being i-th sample.
                   Each datum_i is a dictionary in the form of {feature_k: value_k}
 
     trainingLabels: A list of class lables (integers 0 - 9), of the same length as trainingData
@@ -59,15 +59,35 @@ class SupportVectorMachineClassifier(classificationMethod.ClassificationMethod):
     validationLabels: validation class labels, of the same tyoe as trainingLabels
 
     No returning value is needed in this method.
-    """  
-      
+    """
+
     # might be useful in your code later...
     # this is a list of all features in the training set.
     self.features = list(set([ f for datum in trainingData for f in datum.keys() ]))
 
     "*** YOUR CODE HERE ***"
-  
-  
+    # start with training data
+    formattedTrainingData = self.formattingData(trainingData, self.features)
+
+    # take in two params for svc (gamma and c) s
+    # first find possible ranges for gamma and c
+    # use numpy (np)
+    gammaRange = np.logspace(-9,3,13)
+    cRange = np.logspace(-2,10,13)
+    # search over a grid using quickstart's rbf and linear params
+    # print estimator.get_params().keys()
+    gridToSearch = dict(gamma = gammaRange, C = cRange, kernel = ['rbf', 'linear'])
+    searchedGrid = grid_search.GridSearchCV(svm.SVC(), param_grid = gridToSearch)
+    # fit using input data + labels
+    searchedGrid.fit(formattedTrainingData, trainingLabels)
+    cFit = searchedGrid.best_params_["C"] # grab best fit c value
+    if "gamma" in searchedGrid.best_params_.keys():
+        gammaFit = searchedGrid.best_params_["gamma"]
+        self.clf = svm.SVC(C = cFit, kernel = 'rbf', gamma = gammaFit)
+    else:
+        self.clf = svm.SVC(C = cFit, kernel = searchedGrid.best_params_["kernel"])
+    self.clf.fit(formattedTrainingData, trainingLabels)
+
   def classify(self, testData):
     """
     Use the trained SVM classifier to predict the class labels of testData.
@@ -78,5 +98,7 @@ class SupportVectorMachineClassifier(classificationMethod.ClassificationMethod):
     """
 
     "*** YOUR CODE HERE ***"
-    return [0 for datum in testData]	## blindly return 0 for all instances. DELETE this line before writing your own.
-    
+    # normalize features using formatittingData method
+    formattedData = self.formattingData(testData, self.features)
+    formattedPrediction = self.clf.predict(formattedData)
+    return formattedPrediction
